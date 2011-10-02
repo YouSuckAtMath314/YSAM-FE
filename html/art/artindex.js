b8884f294b0f1b7992b75c4018c12a3c5af0e09e
Jordan Andersen <jordandandersen@gmail.com>
2011-10-01 21:56:54 -0700

    HTML game checkpoint

--- html/art/artindex.js
+++ html/art/artindex.js
@@ -2,7 +2,9 @@ artindex = {
     "background" : { "path": "../bg-battle.png", "frame_height": 1024, "named_frames": {} },
     "background_char" : { "path": "../bg_characterselect.png", "frame_height": 1024, "named_frames": {} },
     "logo_deathmath_nobuttons" : { "path": "../logo_deathmath_nobuttons.png", "frame_height": 1024, "named_frames": {} },
-    "avatar_newton" : { "path": "../avatar_newton.png", "frame_height": 140, "named_frames": {} },
+    "newton" : { "path": "../newton_fight.png", "frame_height": 264, "named_frames": {} },
+    "archimedes" : { "path": "../archimedes_fight.png", "frame_height": 263, "named_frames": {} },
+    "einstein" : { "path": "../einstein_fight.png", "frame_height": 250, "named_frames": {} },
     "button_add" : { "path": "../button_add.png", "frame_height": 140, "named_frames": {} },
     "button_subtract" : { "path": "../button_subtract.png", "frame_height": 140, "named_frames": {} },
     "button_divide" : { "path": "../button_divide.png", "frame_height": 140, "named_frames": {} },
@@ -11,4 +13,7 @@ artindex = {
     "button_subtract_press" : { "path": "../button_subtract_press.png", "frame_height": 140, "named_frames": {} },
     "button_divide_press" : { "path": "../button_divide_press.png", "frame_height": 140, "named_frames": {} },
     "button_multiply_press" : { "path": "../button_multiply_press.png", "frame_height": 140, "named_frames": {} },
+    "bg_health_empty" : { "path": "../bg_health_empty.png", "frame_height": 140, "named_frames": {} },
+    "bg_health_full_red" : { "path": "../bg_health_full_red.png", "frame_height": 140, "named_frames": {} },
+    "bg_health_full_blue" : { "path": "../bg_health_full_blue.png", "frame_height": 140, "named_frames": {} },
 }
